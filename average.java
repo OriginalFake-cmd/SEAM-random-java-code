b58329431838e4b4785eaa3461219a846fe8c0ef
Sulaiman <sulaimansiddiqui55@gmail.com>
2025-06-14 14:57:48 -0500

    Create average.java

---
+++ average.java
@@ -0,0 +1,9 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        double[] myNums = {0.3, 0.4, 0.1, 9.5};
+        System.out.println((myNums[3] + myNums[0] + myNums[1] + myNums[2]) / 4);
+    }
+}
