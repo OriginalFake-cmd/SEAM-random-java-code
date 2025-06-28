ea002191f28166cd4ac82c5815e10243dc65fad7
Sulaiman <sulaimansiddiqui6666@gmail.com>
2025-06-28 18:23:37 -0500

    Create multiply.java

---
+++ multiply.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        
+        int number;
+        if (scanner.hasNextInt()) {
+            number = scanner.nextInt();
+            
+            System.out.println("Multiplication Table for " + number);
+            for (int i = 1; i <= 10; i++) {
+                int result = number * i;
+                System.out.println(number + " x " + i + " = " + result);
+            }
+        } 
+
+        scanner.close();
+    }
+}
